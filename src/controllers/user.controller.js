09d10a40bf398a467b5cd6974be9b9b76619964e
Yash Sinha <Vyash382@gmail.com>
2024-02-04 11:49:48 +0530

    Added more functionalities

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -87,5 +87,12 @@ const loginUser = asyncHandler(async(req,res)=>{
     }
     const {refreshToken,accessToken}=await generateAccessAndRefreshToken(user._id);
     const loggedInUser = await User.findById(user._id).select("-password -refreshToken") ;
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+    return res.status(200).cookie("accessToken",accessToken,options).cookie("refreshToken",refreshToken,options).json(
+        new ApiResponse(200,{user:loggedInUser,accessToken,refreshToken},"User loggedin successfully")
+    )
 });
 export {registerUser,loginUser};
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { loginUser, registerUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multur.model.js";;
 const router = Router()
 router.route("/register").post(
@@ -14,4 +14,5 @@ router.route("/register").post(
         }
     ])
     ,registerUser)
+router.route("/login").post(loginUser)    
 export default router;
